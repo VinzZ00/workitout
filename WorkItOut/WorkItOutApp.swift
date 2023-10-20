1516d3de33ea70ad7b3807ae89863ffbaf89b6a7
Kevin Dallian <kevindallian@gmail.com>
2023-10-20 17:31:23 +0700

    Initial Commit

---
+++ WorkItOut/WorkItOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkItOutApp.swift
+//  WorkItOut
+//
+//  Created by Kevin Dallian on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkItOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
