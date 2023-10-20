3bab10d10a3c8fc0de49a2ed1cb3214fe34b2a78
Elvin <elvin.ri39@gmail.com>
2023-10-21 01:48:51 +0700

    setup Firebase, Adding SDK, setup UIApplicationDelegate

---
+++ WorkItOut/Core/Config/WorkItOutApp.swift
@@ -0,0 +1,31 @@
+//
+//  WorkItOutApp.swift
+//  WorkItOut
+//
+//  Created by Kevin Dallian on 20/10/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct WorkItOutApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
