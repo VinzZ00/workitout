129b0794ec5eb50bd58cfdcc2e531e4d6591628f
Elvin <elvin.ri39@gmail.com>
2023-10-25 16:09:46 +0700

    halfway Assessment Result

---
+++ WorkItOut/WorkItOut/Presentation/Components/AssesmentPage/DayAssessment.swift
@@ -0,0 +1,18 @@
+//
+//  DayAssessment.swift
+//  WorkItOut
+//
+//  Created by Elvin Sestomi on 25/10/23.
+//
+
+import SwiftUI
+
+struct DayAssessment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DayAssessment()
+}
---
+++ WorkItOut/WorkItOut/Presentation/Components/AssesmentPage/workoutListCard.swift
@@ -0,0 +1,70 @@
+//
+//  workoutListCard.swift
+//  WorkItOut
+//
+//  Created by Elvin Sestomi on 25/10/23.
+//
+
+import SwiftUI
+
+
+
+struct workoutListCard: View {
+    
+    var imageAssetName : String
+    var workoutName : String
+    var muscle : String
+    var manySet : Int
+    var manyReps : Int
+    
+    init(imageAssetName: String, workoutName: String, muscle: [String], manySet: Int, manyReps: Int) {
+        self.imageAssetName = imageAssetName
+        self.workoutName = workoutName
+        self.muscle = muscle.joined(separator: ", ");
+        self.manySet = manySet
+        self.manyReps = manyReps
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(imageAssetName)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                    .scaledToFill()
+                    .cornerRadius(6)
+                    .padding(.vertical, 12)
+                    .padding(.leading, 12)
+                VStack(alignment: .leading) {
+                    Text("\(workoutName)")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    Text("\(muscle)")
+                        .foregroundColor(.black)
+                        .font(.body)
+                        
+                }
+                .padding(.vertical, 11)
+                .padding(.leading, 12)
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(manySet) Set")
+                    Text("\(manyReps) Reps")
+                }
+                .padding(.trailing, 12)
+                .padding(.vertical, 12)
+            }
+            .frame(width: 358, height: 88)
+            .padding(.all, 12)
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(.black, lineWidth: 2)
+            )
+        }
+    }
+}
+
+#Preview {
+    workoutListCard(imageAssetName: "PushUP", workoutName: "PushUp", muscle: ["Chess, Hand, Shoulder"], manySet: 12, manyReps: 4)
+}
