ee149291c99c6fcc3879b82a8720165231677ea5
Kevin Dallian <kevindallian@gmail.com>
2023-10-31 15:21:47 +0700

    initialize profile view and viewmodels

---
+++ WorkItOut/WorkItOut/Presentation/ViewModels/Profile/ProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewModel.swift
+//  WorkItOut
+//
+//  Created by Kevin Dallian on 31/10/23.
+//
+
+import Foundation
+
+class ProfileViewModel : ObservableObject {
+    
+}
---
+++ WorkItOut/WorkItOut/Presentation/Views/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  WorkItOut
+//
+//  Created by Kevin Dallian on 31/10/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
