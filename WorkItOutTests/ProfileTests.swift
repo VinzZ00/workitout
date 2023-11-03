e31534c49a1ee15e85123c4dffe159b47761d450
Kevin Dallian <kevindallian@gmail.com>
2023-11-03 13:37:54 +0700

    added test case for profile and history

--- WorkItOutTests/ProfileTests.swift
+++ WorkItOutTests/ProfileTests.swift
@@ -9,25 +9,45 @@ import XCTest
 @testable import WorkItOut
 
 final class ProfileTests: XCTestCase {
-    var profileViewModel = ProfileViewModel()
+    var profileViewModel : ProfileViewModel?
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        profileViewModel = ProfileViewModel()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        profileViewModel = nil
+    }
+    
     func testConvertDaytoStrings(){
         let days : [Day] = [.monday, .tuesday, .sunday]
-        
-        let strings = profileViewModel.convertToString(days: days)
-        XCTAssertEqual(strings, "Mon, Tue, Sun")
+        if let pvm = profileViewModel {
+            let strings = pvm.convertToString(days: days)
+            
+            XCTAssertEqual(strings, "Mon, Tue, Sun")
+        }
     }
     
     func testConvertRelievestoStrings(){
         let relieves : [Relieve] = [.backpain, .breathing, .hippain]
-        let strings = profileViewModel.convertToStrings(relieves: relieves)
+        if let pvm = profileViewModel {
+            let strings = pvm.convertToStrings(relieves: relieves)
+            
+            XCTAssertEqual(strings, "Back Pain, Breathing, Hip Pain")
+        }
         
-        XCTAssertEqual(strings, "Back Pain, Breathing, Hip Pain")
     }
     
     func testConvertGetTrimester(){
         let currentPregnantWeek = 20
-        let strings = profileViewModel.convertToStrings(currentPregnancyWeek: currentPregnantWeek)
-        
-        XCTAssertEqual(strings, "Week 20 (Trimester 2)")
+        if let pvm = profileViewModel {
+            let strings = pvm.convertToStrings(currentPregnancyWeek: currentPregnantWeek)
+            
+            XCTAssertEqual(strings, "Week 20 (Trimester 2)")
+        }
     }
 }
