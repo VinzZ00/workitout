5762f87f4c41d32ad14536dbdc3d5de1e7bcd271
Elvin <elvin.ri39@gmail.com>
2023-11-06 13:52:14 +0700

    updateException Tested

--- WorkItOutTests/WorkItOutTests.swift
+++ WorkItOutTests/WorkItOutTests.swift
@@ -34,7 +34,7 @@ final class WorkitOutTests: XCTestCase {
         try super.tearDownWithError()
     }
 
-    func testExample() async throws {
+    func testAddAndFetchCoreData() async throws {
         if let moc = self.moc {
             
             
@@ -53,7 +53,9 @@ final class WorkitOutTests: XCTestCase {
                         ], day: .monday, estimationDuration: 12, image: "Image 1")
                     ], trimester: .all)
                 ],
-                histories: [])
+                histories: [],
+                exceptions: []
+            );
 
             
             if let testRecord = testRecord {
@@ -64,6 +66,20 @@ final class WorkitOutTests: XCTestCase {
             
             XCTAssertEqual(fetchRes?.first?.name, testRecord!.name, "Done the test")
         }
+    }
+    
+    func testUpdateAddingException() async throws {
+        var fetchRes = await fetchUsecase?.call(context: moc!)
+        
+        var newProfile = fetchRes![fetchRes!.count - 1]
+        newProfile.exceptions.append(Exception.vertigo)
+        
+        await UpdateProfileUseCase().call(profile: newProfile, context: moc!)
+        
+        var afterUpdate = await fetchUsecase?.call(context: moc!)
+        
+        XCTAssertEqual(afterUpdate!.last!.exceptions.map{$0.rawValue}.joined(separator: ", "), newProfile.exceptions.map{$0.rawValue}.joined(separator: ", "), "Done the update test")
+        
     }
 
 }
